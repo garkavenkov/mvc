171d9cd85ee4e016747515e914e997028540ce2a
Maksim Garkavenkov <maksim.garkavenkov@gmail.com>
2017-09-14 13:31:30 +0300

    Add method 'getInstance' for 'Model' class

--- core/Model.php
+++ core/Model.php
@@ -2,7 +2,19 @@
 
 namespace core;
 
+use DBConnector\DBConnect;
+
 abstract class Model
 {
-    
+    protected static function getInstance()
+    {
+        define('DB_USERNAME', 'test');
+        define('DB_PASSWORD', 'test');
+        define('DB_SCHEMA'  , 'test');
+        define('DB_DRIVER'  , 'mysql');
+        define('DB_HOSTNAME', 'localhost');
+
+        $dbh = DBConnect::getInstance();
+        return $dbh;
+    }
 }
